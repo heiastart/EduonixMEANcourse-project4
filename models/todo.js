18064b85a27837fe1527ca645c16331a84f7a470
Oyvind Sorlie <40733351+heiastart@users.noreply.github.com>
2021-03-26 18:26:03 +0100

    Added and coded file for the schema for the todos

---
+++ models/todo.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+// Create a schema object/class for the todos
+var TodosSchema = new mongoose.Schema({
+  title: {type: String, required: true},
+  dueByDate: {type: Date, required: true},
+  createdOn: {type: Date, required: false},
+  status: {type: String, required: true},
+  active: {type: Boolean, required: true},
+  username: {type: String, required: false}
+})
+
+
+// Exporting the model, which will create a collection in the database (in the db_url) with the name toDo (first parameter)
+module.exports = mongoose.model('ToDo', TodosSchema);
